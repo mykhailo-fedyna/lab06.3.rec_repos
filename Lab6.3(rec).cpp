9d29b967e8ccdbc4d51d613ab6cd7411c3d42604
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-06 12:29:21 +0300

    first commit

---
+++ Lab6.3(rec).cpp
@@ -0,0 +1,31 @@
+﻿//lab6.3(rec).cpp
+#include <iostream>
+#include <iomanip>
+using namespace std;
+void Input(int* k, const int size, int i)
+{
+	cout << "k[" << i << "] = ";
+	cin >> k[i];
+
+	if (i < size - 1)
+		Input(k, size, i + 1);
+	else
+		cout << endl;
+}
+void Print(int* k, const int size, int i)
+{
+	cout << setw(4) << k[i];
+	if (i < size - 1)
+		Print(k, size, i + 1);
+	else
+		cout << endl;
+}
+
+int main()
+{
+	const int n = 6;
+	int k[n];
+	Input(k, n, 0);
+	Print(k, n, 0);
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <sstream>
+#include "../lab6.3(rec).cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(TestInput)
+		{
+			const int size = 6;
+			int k[size];
+			int expected[size] = { 1, 2, 3, 4, 5, 6 };
+			for (int i = 0; i < size; i++)
+			{
+				k[i] = expected[i];
+			}
+			for (int i = 0; i < size; i++)
+			{
+				Assert::AreEqual(expected[i], k[i]);
+			}
+		}
+		TEST_METHOD(TestPrint)
+		{
+			const int size = 6;
+			int k[size] = { 1, 2, 3, 4, 5, 6 };
+			stringstream buffer;
+			streambuf* prevcoutbuf = cout.rdbuf(buffer.rdbuf());
+			Print(k, size, 0);
+			cout.rdbuf(prevcoutbuf);
+			Assert::AreEqual(buffer.str(), string("   1   2   3   4   5   6\n"));
+		}
+	};
+}
+
